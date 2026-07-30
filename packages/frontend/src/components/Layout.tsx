3fb322cfc93810e46d368e3bd6d40baba44f1fcf
Eduardo Pacheco <eduardo@eapprojetos.com.br>
2026-07-30 11:13:49 -0300

    feat: menu modules with icons in header

--- packages/frontend/src/components/Layout.tsx
+++ packages/frontend/src/components/Layout.tsx
@@ -5,10 +5,18 @@ import {
   Toolbar,
   Typography,
   Button,
+  IconButton,
   Menu,
   MenuItem,
+  ListItemIcon,
+  ListItemText,
   Box,
 } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import PersonIcon from '@mui/icons-material/Person'
+import WorkIcon from '@mui/icons-material/Work'
+import SendIcon from '@mui/icons-material/Send'
+import DescriptionIcon from '@mui/icons-material/Description'
 import { useAuth } from '../contexts/AuthContext'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -24,22 +32,31 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     handleClose()
   }
 
+  const items = [
+    { label: 'Freelancers', path: '/freelancers', icon: <PersonIcon /> },
+    { label: 'Jobs', path: '/freelancers?tab=1', icon: <WorkIcon /> },
+    { label: 'Proposals', path: '/freelancers?tab=2', icon: <SendIcon /> },
+    { label: 'Contracts', path: '/freelancers?tab=3', icon: <DescriptionIcon /> },
+  ]
+
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <Button color="inherit" onClick={handleMenu} sx={{ mr: 2 }}>
-            Freelancer
-          </Button>
+          <IconButton color="inherit" edge="start" onClick={handleMenu} sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
           <Menu
             anchorEl={anchorEl}
             open={!!anchorEl}
             onClose={handleClose}
           >
-          <MenuItem onClick={() => go('/freelancers')}>Freelancers</MenuItem>
-          <MenuItem onClick={() => go('/freelancers?tab=1')}>Jobs</MenuItem>
-          <MenuItem onClick={() => go('/freelancers?tab=2')}>Proposals</MenuItem>
-          <MenuItem onClick={() => go('/freelancers?tab=3')}>Contracts</MenuItem>
+            {items.map((item) => (
+              <MenuItem key={item.path} onClick={() => go(item.path)}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText>{item.label}</ListItemText>
+              </MenuItem>
+            ))}
           </Menu>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             App
